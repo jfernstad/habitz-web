9909854e8317c9da235b61bea92aba5f0382c099
Joakim Fernstad <joakim.fernstad@gmail.com>
2022-05-23 22:18:47 +0200

    Updated html for API changes, allow schedule editing

--- html/habitz.js
+++ html/habitz.js
@@ -1,44 +1,53 @@
 var baseURL = "http://localhost:3000" // TODO: Figure out a good way to swap dev/prod URLs
 
-function POST(url, payload, callback, errCallback = null) {
-    var request = new XMLHttpRequest();
-    // request.withCredentials = true;
-    request.open("POST", baseURL + url);
-    request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    request.setRequestHeader("Authorization", "Bearer " + localStorage.getItem("habitz-token"))
-
-    request.onreadystatechange = function () {
-        if (this.readyState == 4) {
-            if (this.status >= 200 && this.status <= 299) {
-                callback(this.status, this.responseText)
-            }
-            else if (this.status >= 400 && errCallback != null) {
-                errCallback(this.status, this.responseText)
-            }
-        }
-    };
-    request.send(JSON.stringify(payload));
-}
-
+//
+// Backend requests wiith JWT authentication and JSON payload
+//
 function GET(url, callback, errCallback = null) {
-    var request = new XMLHttpRequest();
-    // request.withCredentials = true;
-    request.open("GET", baseURL + url);
-    request.setRequestHeader("Authorization", "Bearer " + localStorage.getItem("habitz-token"))
-
-    request.onreadystatechange = function () {
-        if (this.readyState == 4) {
-            if (this.status >= 200 && this.status <= 299) {
-                callback(this.status, this.responseText)
-            }
-            else if (this.status >= 400 && errCallback != null) {
-                errCallback(this.status, this.responseText)
-            }
-        }
-    };
+    var request = _baseRequest("GET", url, callback, errCallback);
     request.send();
 }
 
+function POST(url, payload, callback, errCallback = null) {
+    var request = _baseRequest("POST", url, callback, errCallback);
+    request.send(JSON.stringify(payload));
+}
+
+function PATCH(url, payload, callback, errCallback = null) {
+    var request = _baseRequest("PATCH", url, callback, errCallback);
+    request.send(JSON.stringify(payload));
+}
+
+function DELETE(url, payload, callback, errCallback = null) {
+    var request = _baseRequest("DELETE", url, callback, errCallback);
+    request.send(JSON.stringify(payload));
+    // The astute reader has identified that the DELETE verb 
+    // should not have a payload. However, the spec does not
+    // explicetly forbid it, and the golang package go-chi
+    // allows it, therefore so will I. :) 
+}
+
+function _baseRequest(method, url, callback, errCallback = null){
+    var request = new XMLHttpRequest();
+    request.open(method, baseURL + url);
+    request.setRequestHeader("Authorization", "Bearer " + localStorage.getItem("habitz-token"))
+
+    request.onreadystatechange = function () {
+        if (this.readyState == 4) {
+            if (this.status >= 200 && this.status <= 299) {
+                callback(this.status, this.responseText)
+            }
+            else if (this.status >= 400 && errCallback != null) {
+                errCallback(this.status, this.responseText)
+            }
+        }
+    };
+    return request
+}
+
+//
+// Convenience functions
+//
 function userFirstname(){
     token = localStorage.getItem("habitz-token")
     claims = parseJwt(token)
